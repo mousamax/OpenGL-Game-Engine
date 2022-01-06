ad99cb9b8559ce2505410bedd7df56d818444e86
Mousa Mohammad Abdulmaxod <49567708+MousaMohammad@users.noreply.github.com>
2022-01-06 14:12:49 +0200

    added Game-Manager

--- source/common/systems/game-manager.hpp
+++ source/common/systems/game-manager.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "../ecs/world.hpp"
-#include "../components/movement.hpp"
 
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
@@ -11,26 +10,37 @@
 namespace our
 {
 
-    // The movement system is responsible for moving every entity which contains a MovementComponent.
-    // This system is added as a simple example for how use the ECS framework to implement logic. 
-    // For more information, see "common/components/movement.hpp"
-    class MovementSystem {
-    public:
+    // The Game Manager system is responsible for controling the game logic.
+    // keep track of the score, time etc... 
+    class GameMananger {
+    private:
+        int score = 0;
 
+    public:
+        //this static object can be used by different logic components 
+        static GameMananger gm;
+
+        //used to add/subtract score ex:coin collection or hit obstacle
+        void changeScore(int value)
+        {
+            score += value;
+        }
         // This should be called every frame to update all entities containing a MovementComponent. 
+        /*
         void update(World* world, float deltaTime) {
             // For each entity in the world
-            for(auto entity : world->getEntities()){
+            for (auto entity : world->getEntities()) {
                 // Get the movement component if it exists
                 MovementComponent* movement = entity->getComponent<MovementComponent>();
                 // If the movement component exists
-                if(movement){
+                if (movement) {
                     // Change the position and rotation based on the linear & angular velocity and delta time.
                     entity->localTransform.position += deltaTime * movement->linearVelocity;
                     entity->localTransform.rotation += deltaTime * movement->angularVelocity;
                 }
             }
         }
+        */
     };
 
 }
