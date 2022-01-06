b222bb0f609129f5a027abaccd0e9d8df941c99c
Mousa Mohammad Abdulmaxod <49567708+MousaMohammad@users.noreply.github.com>
2022-01-06 16:20:36 +0200

    change state

--- source/common/systems/game-manager.hpp
+++ source/common/systems/game-manager.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../ecs/world.hpp"
+#include "../application.hpp"
 
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
@@ -10,37 +11,55 @@
 namespace our
 {
 
-    // The Game Manager system is responsible for controling the game logic.
-    // keep track of the score, time etc... 
-    class GameMananger {
-    private:
-        int score = 0;
+	// The Game Manager system is responsible for controling the game logic.
+	// keep track of the score, time etc... 
+	class GameMananger {
 
-    public:
-        //this static object can be used by different logic components 
-        static GameMananger gm;
+		int score = 0;
+		Application* app; // The application in which the state runs
+		enum states { MENU, PLAY, GAMEOVER };
+		states currentState;
 
-        //used to add/subtract score ex:coin collection or hit obstacle
-        void changeScore(int value)
-        {
-            score += value;
-        }
-        // This should be called every frame to update all entities containing a MovementComponent. 
-        /*
-        void update(World* world, float deltaTime) {
-            // For each entity in the world
-            for (auto entity : world->getEntities()) {
-                // Get the movement component if it exists
-                MovementComponent* movement = entity->getComponent<MovementComponent>();
-                // If the movement component exists
-                if (movement) {
-                    // Change the position and rotation based on the linear & angular velocity and delta time.
-                    entity->localTransform.position += deltaTime * movement->linearVelocity;
-                    entity->localTransform.rotation += deltaTime * movement->angularVelocity;
-                }
-            }
-        }
-        */
-    };
+	public:
+		states menu_state = MENU;
+		states play_state = PLAY;
+		states gameover_state = GAMEOVER;
+
+		//this static object can be used by different logic components 
+		static GameMananger gm;
+
+		// When a state enters, it should call this function and give it the pointer to the application
+		void enter(Application* app, states state) {
+			this->app = app;
+			currentState = state;
+		}
+		//used to add/subtract score ex:coin collection or hit obstacle
+		void changeScore(int value)
+		{
+			score += value;
+		}
+		void startGame()
+		{
+			app->changeState("main");
+		}
+		// This should be called every frame to update all entities containing a MovementComponent. 
+		void update(World* world) {
+
+			if (app->getKeyboard().isPressed(GLFW_KEY_ESCAPE))
+			{
+				switch (currentState)
+				{
+				case our::GameMananger::MENU:
+					app->changeState("main");
+					break;
+				case our::GameMananger::PLAY:
+					app->changeState("menu");
+					break;
+				default:
+					break;
+				}
+			}
+		}
+	};
 
 }
--- source/states/menu-state.hpp
+++ source/states/menu-state.hpp
@@ -3,53 +3,54 @@
 #include <application.hpp>
 
 #include <ecs/world.hpp>
-#include <systems/game-manager.hpp>
 #include <systems/forward-renderer.hpp>
-#include <systems/free-camera-controller.hpp>
-#include <systems/movement.hpp>
+#include <systems/game-manager.hpp>
 #include <asset-loader.hpp>
 
 #include <stdint.h>
 
-// This state shows how to use the ECS framework and deserialization.
-class Playstate: public our::State {
+// This state for the menu.
+class MenuState : public our::State {
 
     our::World world;
-    our::GameMananger gameManager;
     our::ForwardRenderer renderer;
-    our::FreeCameraControllerSystem cameraController;
-    our::MovementSystem movementSystem;
+    our::GameMananger gameManager;
 
     void onInitialize() override {
         // First of all, we get the scene configuration from the app config
         auto& config = getApp()->getConfig()["scene"];
         // If we have assets in the scene config, we deserialize them
-        if(config.contains("assets")){
+        if (config.contains("assets")) {
             our::deserializeAllAssets(config["assets"]);
         }
         // If we have a world in the scene config, we use it to populate our world
-        if(config.contains("world")){
+        if (config.contains("world")) {
             world.deserialize(config["world"]);
         }
-        // We initialize the camera controller system since it needs a pointer to the app
-        cameraController.enter(getApp());
-        gameManager.enter(getApp(),gameManager.play_state);
+        gameManager.enter(getApp(), gameManager.menu_state);
     }
 
     void onDraw(double deltaTime) override {
-        // Here, we just run a bunch of systems to control the world logic
-        movementSystem.update(&world, (float)deltaTime);
-        cameraController.update(&world, (float)deltaTime);
-        gameManager.update(&world);
+        
         // And finally we use the renderer system to draw the scene
         auto size = getApp()->getFrameBufferSize();
+        gameManager.update(&world);
         renderer.render(&world, glm::ivec2(0, 0), size);
     }
 
     void onDestroy() override {
-        // On exit, we call exit for the camera controller system to make sure that the mouse is unlocked
-        cameraController.exit();
         // and we delete all the loaded assets to free memory on the RAM and the VRAM
         our::clearAllAssets();
+    }
+
+    void onImmediateGui() {
+
+        ImGui::Begin("Game Menu");
+
+        if (ImGui::Button("Reset")) {
+           
+        }
+
+        ImGui::End();
     }
 };
