52e34eaf84a6095257227e10cf63b1a4468031e2
Mousa Mohammad Abdulmaxod <mousa.abdulmaxod001@eng-st.cu.edu.eg>
2022-01-07 23:45:09 +0200

    added steer velocity

--- source/common/components/player-movement.hpp
+++ source/common/components/player-movement.hpp
@@ -12,6 +12,7 @@ namespace our
     public:
         float horizontalSensitivity = 3.0f; // How sensitive the keyboard is to A & D
         float forwardVelocity = 3.0f;       // how quick does the player move forward
+        float steeringVelocity = 7.0f;       // how quick does the player steer
 
         // The ID of this component type is "Player Movement"
         static std::string getID() { return "Player Movement"; }
