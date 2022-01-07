67a5ac89177f10049e8545528015dba7d719a7a4
Abdelrhman-Hosny <ahosny2792@gmail.com>
2022-01-07 16:42:20 +0200

    formatting

--- source/common/components/free-camera-controller.cpp
+++ source/common/components/free-camera-controller.cpp
@@ -2,10 +2,13 @@
 #include "../ecs/entity.hpp"
 #include "../deserialize-utils.hpp"
 
-namespace our {
+namespace our
+{
     // Reads sensitivities & speedupFactor from the given json object
-    void FreeCameraControllerComponent::deserialize(const nlohmann::json& data){
-        if(!data.is_object()) return;
+    void FreeCameraControllerComponent::deserialize(const nlohmann::json &data)
+    {
+        if (!data.is_object())
+            return;
         rotationSensitivity = data.value("rotationSensitivity", rotationSensitivity);
         fovSensitivity = data.value("fovSensitivity", fovSensitivity);
         positionSensitivity = data.value("positionSensitivity", positionSensitivity);
